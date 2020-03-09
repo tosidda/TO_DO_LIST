504c1b40ac535bb20b2565f6ae7e99b9f6dce0f0
Sidda Chalamalasetty <tosidda@gmail.com>
2020-03-09 16:23:48 -0400

    Updates

---
+++ precache-manifest.654660379002ec4db80dc9b914d6126e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059200b652310c4b8ec4fa8f25613305",
+    "url": "/TO_DO_LIST/index.html"
+  },
+  {
+    "revision": "98fbe03065cba7283933",
+    "url": "/TO_DO_LIST/static/css/main.4947d61b.chunk.css"
+  },
+  {
+    "revision": "11d8831ff7b239811c38",
+    "url": "/TO_DO_LIST/static/js/2.0e42cc80.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TO_DO_LIST/static/js/2.0e42cc80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98fbe03065cba7283933",
+    "url": "/TO_DO_LIST/static/js/main.795cf9ea.chunk.js"
+  },
+  {
+    "revision": "931469ff032234eeb411",
+    "url": "/TO_DO_LIST/static/js/runtime-main.49471e29.js"
+  }
+]);
